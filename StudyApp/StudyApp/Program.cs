602d610406f5412a71bb6a1c075f146eb1f41825
Alyne MR Soares <35814001+AlyneSoares@users.noreply.github.com>
2019-05-08 18:02:21 -0300

    Program

---
+++ StudyApp/StudyApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyApp
+{
+    namespace StudyApp
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                string message1 = "type a number - not zero";
+                Console.WriteLine(message1);
+
+                float n = float.Parse(Console.ReadLine());
+
+                string resposta = "";
+                for (int i = 2; resposta != "y"; i++)
+                {
+                    Console.WriteLine(n + " é maior que 1000? Y / N ? ");
+                    resposta = Console.ReadLine();
+
+                    if (n % i == 0)
+                    {
+                        n = n / i;                        
+                    }
+                    else
+                    {
+                        n = n * i;
+                    }
+
+                    Console.WriteLine(n);
+                    string result = n % 2 == 0 ? "par" : "impar";
+
+                    Console.WriteLine(result);
+                }
+                Console.WriteLine("Missão Cumprida");                                              
+            }            
+        }
+    }
+}
