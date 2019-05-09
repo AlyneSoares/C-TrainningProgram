03e51a43c82365ccbb3002f9e961c110367ef1cb
Soares <Alyne_Soares@Dellteam.com>
2019-05-09 10:37:07 -0300

    confirming input isNumber

--- StudyApp/StudyApp/Program.cs
+++ StudyApp/StudyApp/Program.cs
@@ -1,44 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace StudyApp
 {
-    namespace StudyApp
+    public class Program
     {
-        class Program
+        public static void Main(string[] args)
         {
-            static void Main(string[] args)
+            string resposta = "";
+            int number = 0;
+            
+            Console.WriteLine("type a number - not zero");
+
+            while (true)
             {
-                string message1 = "type a number - not zero";
-                Console.WriteLine(message1);
-
-                float n = float.Parse(Console.ReadLine());
-
-                string resposta = "";
-                for (int i = 2; resposta != "y"; i++)
+                try
                 {
-                    Console.WriteLine(n + " é maior que 1000? Y / N ? ");
-                    resposta = Console.ReadLine();
-
-                    if (n % i == 0)
-                    {
-                        n = n / i;                        
-                    }
-                    else
-                    {
-                        n = n * i;
-                    }
-
-                    Console.WriteLine(n);
-                    string result = n % 2 == 0 ? "par" : "impar";
-
-                    Console.WriteLine(result);
+                    number = int.Parse(Console.ReadLine());
+                    break;
                 }
-                Console.WriteLine("Missão Cumprida");                                              
-            }            
-        }
+                catch
+                {
+                    Console.WriteLine("Try again");
+                }
+            }
+
+            for (int i = 2; resposta != "y"; i++)
+            {
+                Console.WriteLine("{0} é maior que 1000? Y / N ? ", number);
+                resposta = Console.ReadLine();
+
+                if (number % i == 0)
+                {
+                    number /= i;                        
+                }
+                else
+                {
+                    number *= i;
+                }
+
+                Console.WriteLine(number);
+
+                string result = number % 2 == 0 ? number + " é par" : number + " é impar";
+                Console.WriteLine(result);
+            }
+            Console.WriteLine("Missão Cumprida");                                              
+        }            
     }
 }
